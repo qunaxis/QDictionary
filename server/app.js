18c23040e97d369c901b8170b7cdad4e9711ef22
Dmitry Podlesnykh <qunaxis@gmail.com>
2017-01-02 01:21:15 +0300

    Ready to development

--- server/app.js
+++ server/app.js
@@ -3,26 +3,35 @@ import morgan from 'morgan'
 import bodyParser from 'body-parser'
 import mongoose from 'mongoose'
 
+import webpack from 'webpack'
+import webpackDevMiddleware from 'webpack-dev-middleware'
+import webpackHotMiddleware from 'webpack-hot-middleware'
+import webpackconfig from '../webpack.config'
+
 import apiRoutes from './apiRoutes'
 import * as db from './utils/db'
 
 
 import config from './cfg.json'
 
-let app = express();
+let app = express()
 
-app.use(morgan('dev'));
+let compiler = webpack(webpackconfig)
+app.use(webpackDevMiddleware(compiler, { noInfo: true, publicPath: webpackconfig.output.publicPath }))
+app.use(webpackHotMiddleware(compiler))
 
-db.connection();
+app.use(morgan('dev'))
 
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+db.connection()
 
-app.use('/', (req, res) => {
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
 
-});
-app.use('/api/v1/', apiRoutes);
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html')
+})
+app.use('/api/v1/', apiRoutes)
 
 app.listen(config.server.port, err => {
-    err ?  console.error(err) : console.log(`Server has started on ${config.server.port}`);
-});
+    err ?  console.error(err) : console.log(`Server has started on ${config.server.port}`)
+})
